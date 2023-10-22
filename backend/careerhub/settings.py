6cff587a3c7bd0e6904f76bfdf995c1e5b758dfe
Danil Raspopov <anarant91@gmail.com>
2023-10-22 18:46:16 +0300

    remove migrations

--- backend/careerhub/settings.py
+++ backend/careerhub/settings.py
@@ -95,7 +95,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = "users.User"
+AUTH_USER_MODEL = 'users.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
