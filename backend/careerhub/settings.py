69b3b12318e0f0153527b449a8faf216a0c0e3d4
Danil Raspopov <anarant91@gmail.com>
2023-10-22 21:12:53 +0300

    create new usermodel

--- backend/careerhub/settings.py
+++ backend/careerhub/settings.py
@@ -1,7 +1,9 @@
 import os
 from pathlib import Path
-
 from django.core.management.utils import get_random_secret_key
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
