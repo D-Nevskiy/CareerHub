901a5b62b98de2f0cbd59412bebdc48c5ec9a74d
Danil Raspopov <anarant91@gmail.com>
2023-10-24 16:13:13 +0300

    setting smtp

--- backend/careerhub/settings.py
+++ backend/careerhub/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
-DEBUG = os.getenv('DEBUG', 'False') == 'False'
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(' ')
 
