cf4c9cba8ae5abcea3a894ee2a4ae3aca37c3608
Danil Raspopov <anarant91@gmail.com>
2023-10-25 20:39:11 +0300

    add app sharedinfo and fix createsuperuser

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,30 +1,29 @@
 import requests
 from djoser.views import UserViewSet
-from rest_framework import viewsets
-from rest_framework.decorators import action, permission_classes
-from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny
 
 from users.models import User
 from users.serializers import CustomUserSerializer
 
 
 class CustomUserViewSet(UserViewSet):
-    queryset = User.objects.all()
-    serializer_class = CustomUserSerializer
 
-
-
-class EmailViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = CustomUserSerializer
     permission_classes = [AllowAny]
 
-    @action(["get"], detail=False)
-    def activate(self, request, uid, token):
+    @action(
+        methods=['get'], detail=False,
+        url_path='activation_user', permission_classes=(AllowAny,)
+    )
+    def activate(self, request, uid, token, format=None):
+        print(request)
         payload = {'uid': uid, 'token': token}
 
-        url = "http://localhost:8000/api/auth/users/activation/"
+        url = "http://localhost:8000/api/users/activation/"
         response = requests.post(url, data=payload)
 
         if response.status_code == 204:
